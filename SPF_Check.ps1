3d6411b6386942e6bfa3d7744168523a076c602a
Suresh Sivasankaran <47207504+sureshwin006@users.noreply.github.com>
2023-03-17 11:43:40 +0530

    Delete SPF_Check.ps1

--- SPF_Check.ps1
+++
@@ -1,6 +0,0 @@
-$IMDomainList = Import-CSV -Path "C:\Accepted_Domains_List_O365.csv"
-
-Foreach ($domain in $IMDomainList) 
-    { 
-    Get-SPFRecord -Name $domain.Domains |  Export-CSV -Append "C:\SPF_Record.csv" -NoTypeInformation
-    }
