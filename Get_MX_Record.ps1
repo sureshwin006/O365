558da7372c729eae9d7050acbf2da27db3fde71a
Suresh Sivasankaran <47207504+sureshwin006@users.noreply.github.com>
2023-03-17 11:43:34 +0530

    Delete Get_MX_Record.ps1

--- Get_MX_Record.ps1
+++
@@ -1,6 +0,0 @@
-$IMDomainList = Import-CSV -Path "C:\Accepted_Domains_List_O365.csv"
-
-Foreach ($domain in $IMDomainList)
-    { 
-    Resolve-DnsName -Name $domain.Domains -Type MX | Export-CSV -Append "C:\MX_Record.csv" -NoTypeInformation -Force
-    }
