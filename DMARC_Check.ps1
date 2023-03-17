f91a8c4ac5f537c4ee7f158af1027dfea796825d
Suresh Sivasankaran <47207504+sureshwin006@users.noreply.github.com>
2023-03-17 11:43:28 +0530

    Delete DMARC_Check.ps1

--- DMARC_Check.ps1
+++
@@ -1,6 +0,0 @@
-$IMDomainList = Import-CSV -Path "C:\Accepted_Domains_List_O365.csv"
-
-Foreach ($domain in $IMDomainList) 
-    { 
-    Get-DMARCRecord -Name $domain.Domains |  Export-CSV -Append "C:\DMARCRecord.csv" -NoTypeInformation
-    }
